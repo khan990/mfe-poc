c18f57456041fa15c4fc8d11d65f8f4f378e9cb4
Jasim Khan Afridi <khan990@gmail.com>
2023-06-28 18:50:09 +0200

    chore: generate angular-mfe-1

---
+++ packages/angular-mfe-1/module-federation.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  name: 'angular-mfe-1',
+  exposes: {
+    './Module': 'packages/angular-mfe-1/src/app/remote-entry/entry.module.ts',
+  },
+};
--- packages/shell/module-federation.config.js
+++ packages/shell/module-federation.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   name: 'shell',
-  remotes: ['landing-page', 'react-mfe-1', 'react-mfe-2', 'react-mfe-3'],
+  remotes: [
+    'landing-page',
+    'react-mfe-1',
+    'react-mfe-2',
+    'react-mfe-3',
+    'angular-mfe-1',
+  ],
 };
