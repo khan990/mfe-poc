edab9eeab6e9c5adf1f14f5d9f8e4ec9cd8081ae
Jasim Khan Afridi <khan990@gmail.com>
2023-06-28 18:45:07 +0200

    chore: react landing page mfe

--- packages/shell/src/app/app.tsx
+++ packages/shell/src/app/app.tsx
@@ -4,6 +4,8 @@ import NxWelcome from './nx-welcome';
 
 import { Link, Route, Routes } from 'react-router-dom';
 
+const LandingPage = React.lazy(() => import('landing-page/Module'));
+
 export function App() {
   return (
     <React.Suspense fallback={null}>
@@ -11,9 +13,13 @@ export function App() {
         <li>
           <Link to="/">Home</Link>
         </li>
+        <li>
+          <Link to="/landing-page">LandingPage</Link>
+        </li>
       </ul>
       <Routes>
         <Route path="/" element={<NxWelcome title="shell" />} />
+        <Route path="/landing-page" element={<LandingPage />} />
       </Routes>
     </React.Suspense>
   );
