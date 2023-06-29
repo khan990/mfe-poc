e2c1f289b8b2e4ee08ddf5b87750920db9dc6868
Jasim Khan Afridi <khan990@gmail.com>
2023-06-29 21:13:12 +0200

    refactor: remove example code

--- packages/shell/src/app/app.tsx
+++ packages/shell/src/app/app.tsx
@@ -1,16 +1,24 @@
 import * as React from 'react';
 
-import NxWelcome from './nx-welcome';
-
 import { Link, Route, Routes } from 'react-router-dom';
 
-const ReactMfe3 = React.lazy(() => import('react-mfe-3/Module'));
-
-const ReactMfe2 = React.lazy(() => import('react-mfe-2/Module'));
-
+const AngularMfe1 = React.lazy(() => import('angular-mfe-1/Module'));
+const AngularMfe2 = React.lazy(() => import('angular-mfe-2/Module'));
+const AngularMfe3 = React.lazy(() => import('angular-mfe-3/Module'));
 const ReactMfe1 = React.lazy(() => import('react-mfe-1/Module'));
-
+const ReactMfe2 = React.lazy(() => import('react-mfe-2/Module'));
+const ReactMfe3 = React.lazy(() => import('react-mfe-3/Module'));
 const LandingPage = React.lazy(() => import('landing-page/Module'));
+
+
+
+// const AngularMfe1 = import('angular-mfe-1/Module');
+// const AngularMfe2 = import('angular-mfe-2/Module');
+// const AngularMfe3 = import('angular-mfe-3/Module');
+// const ReactMfe1 = import('react-mfe-1/Module');
+// const ReactMfe2 = import('react-mfe-2/Module');
+// const ReactMfe3 = import('react-mfe-3/Module');
+// const LandingPage = import('landing-page/Module');
 
 export function App() {
   return (
@@ -31,13 +39,36 @@ export function App() {
         <li>
           <Link to="/landing-page">LandingPage</Link>
         </li>
+        <li>
+          <Link to="/angular-mfe-1">AngularMfe1</Link>
+        </li>
+        <li>
+        <Link to="/angular-mfe-2">AngularMfe2</Link>
+        </li>
+        <li>
+        <Link to="/angular-mfe-3">AngularMfe3</Link>
+        </li>
       </ul>
+      <hr />
+      <br />
+      <br />
+      <ReactMfe1 />
+      <ReactMfe2 />
+      <ReactMfe3 />
+      {/* <AngularMfe1 /> */}
+      {/* <AngularMfe2 />
+      <AngularMfe3 /> */}
+      <LandingPage />
+
       <Routes>
-        <Route path="/" element={<NxWelcome title="shell" />} />
+        {/* <Route path="/" element={<App />} /> */}
         <Route path="/react-mfe-3" element={<ReactMfe3 />} />
         <Route path="/react-mfe-2" element={<ReactMfe2 />} />
         <Route path="/react-mfe-1" element={<ReactMfe1 />} />
         <Route path="/landing-page" element={<LandingPage />} />
+        <Route path="/angular-mfe-1" element={<AngularMfe1 />} />
+        <Route path="/angular-mfe-2" element={<AngularMfe2 />} />
+        <Route path="/angular-mfe-3" element={<AngularMfe3 />} />
       </Routes>
     </React.Suspense>
   );
--- packages/shell/webpack.config.js
+++ packages/shell/webpack.config.js
@@ -6,6 +6,15 @@ const baseConfig = require('./module-federation.config');
 
 const config = {
   ...baseConfig,
+  remotes: [
+    ['landing-page', 'http://localhost:4201/remoteEntry.js'],
+    ['react-mfe-1', 'http://localhost:4202/remoteEntry.js'],
+    ['react-mfe-2', 'http://localhost:4203/remoteEntry.js'],
+    ['react-mfe-3', 'http://localhost:4204/remoteEntry.js'],
+    ['angular-mfe-1', 'http://localhost:4205/remoteEntry.mjs'],
+    ['angular-mfe-2', 'http://localhost:4206/remoteEntry.mjs'],
+    ['angular-mfe-3', 'http://localhost:4207/remoteEntry.mjs'],
+  ]
 };
 
 // Nx plugins for webpack to build config object from Nx options and context.
