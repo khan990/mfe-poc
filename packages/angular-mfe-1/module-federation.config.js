dbe02d1d9cc37b63f6bd667d6d1eaa90e69eaa5d
Jasim Khan Afridi <jasim.khan@deliveryhero.com>
2023-07-04 19:38:16 +0200

    chore: remove ng-mfe-1

--- packages/angular-mfe-1/module-federation.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  name: 'angular-mfe-1',
-  exposes: {
-    './Module': 'packages/angular-mfe-1/src/app/remote-entry/entry.module.ts',
-  },
-};
--- packages/angular-mfe-1/src/app/remote-entry/nx-welcome.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-
-@Component({
-  selector: 'app-nx-welcome',
-  template: `<h1>Welcome angular-mfe-1.</h1>`,
-  styles: [],
-  encapsulation: ViewEncapsulation.None,
-})
-export class NxWelcomeComponent {}
