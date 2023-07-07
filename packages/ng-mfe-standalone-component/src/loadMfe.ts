ae91e7d17b7297af39229b2a943429d3605b7c4d
Jasim Khan Afridi <jasim.khan@deliveryhero.com>
2023-07-07 15:37:13 +0200

    chore: remove ng-mfe-standalone-component

--- packages/ng-mfe-standalone-component/src/loadMfe.ts
+++
@@ -1,9 +0,0 @@
-import 'zone.js';
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-
-const mountStandalone = () => {
-  bootstrapApplication(AppComponent); // https://angular.io/api/platform-browser/bootstrapApplication
-};
-
-export { mountStandalone };
